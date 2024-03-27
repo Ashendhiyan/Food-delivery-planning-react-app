d10dd3f2c2b9930fc7496a401cf85f6ffd0ae47f
ashenIntelleon <ashen.p@intelleon.com>
2024-03-27 15:10:32 +0530

    crete loading method when home page refresh

--- frontend/src/component/HomeCard.tsx
+++ frontend/src/component/HomeCard.tsx
@@ -1,23 +1,44 @@
-import React from 'react'
+import React from "react";
 
 interface HomeCardProps {
-    name: string;
-    image: string;
-    category: string;
-    price: number;
-  }
-
-const HomeCard: React.FC<HomeCardProps> = ({name,image,category,price}) => {
-  return (
-    <div className='bg-white shadow-md p-2 rounded'>
-        <div className='w-40 min-h-[150px]'>
-            <img src={image} className='h-full w-full'/>
-        </div>
-        <h3 className='font-semibold text-slate-600 text-center capitalize text-lg'>{name}</h3>
-        <p className='text-center text-slate-500 font-medium'>{category}</p>
-        <p className='text-center font-bold'><span className='text-red-500'>Rs.</span><span>{price}</span></p>
-    </div>
-  )
+  name: string;
+  image: string;
+  category: string;
+  price: number;
+  loading : string;
 }
 
-export default HomeCard
+const HomeCard: React.FC<HomeCardProps> = ({
+  name,
+  image,
+  category,
+  price,
+  loading
+}) => {
+  return (
+    <div className="bg-white shadow-md p-2 rounded min-w-[150px]">
+      {name ? (
+        <>
+          <div className="w-40 min-h-[150px]">
+            <img src={image} className="h-full w-full" />
+          </div>
+          <h3 className="font-semibold text-slate-600 text-center capitalize text-lg">
+            {name}
+          </h3>
+          <p className="text-center text-slate-500 font-medium">{category}</p>
+          <p className="text-center font-bold">
+            <span className="text-red-500">Rs.</span>
+            <span>{price}</span>
+          </p>
+        </>
+      )
+    :
+    <div className="flex justify-center items-center h-full">
+        <p>{loading}</p>
+    </div>
+    }
+    </div>
+  );
+};
+
+export default HomeCard;
--- frontend/src/page/Home.tsx
+++ frontend/src/page/Home.tsx
@@ -1,65 +1,90 @@
-import React from 'react'
-import cycle from "../assets/cycling_983534.png"
-import HomeCard from '../component/HomeCard'
-import { useSelector } from 'react-redux'
-import CardFeature from '../component/CardFeature'
+import React from "react";
+import cycle from "../assets/cycling_983534.png";
+import HomeCard from "../component/HomeCard";
+import { useSelector } from "react-redux";
+import CardFeature from "../component/CardFeature";
 
-const Home:React.FC = () => {
-  const productData = useSelector((state:any)=>state.product.productList)
-  console.log(productData)
-  const homeProductCartList = productData.slice(0,4)
+const Home: React.FC = () => {
+  const productData = useSelector((state: any) => state.product.productList);
+  console.log(productData);
+  const homeProductCartList = productData.slice(0, 4);
+
   // Vegitables filtering part
-  const homeProductCartListVegitables = productData.filter((el:any)=>el.category === "Vegitables",[])
-  console.log(homeProductCartListVegitables)
+  const homeProductCartListVegitables = productData.filter(
+    (el: any) => el.category === "Vegitables",
+    []
+  );
+  console.log(homeProductCartListVegitables);
+
+  //Loding Array
+  const loadingArray = new Array(4).fill(null);
+
   return (
-    <div className='p-2 md:p-4'>
-      <div className='md:flex gap-5 py-2'>
-        <div className='md:w-1/2'>
-          <div className='flex gap-3 bg-slate-300 w-36 px-2 items-center rounded-full'>
-              <p className='text-sm font-medium text-slate-900'>Bike Delivery</p>
-              <img src={cycle} className='h-7'/>
+    <div className="p-2 md:p-4">
+      <div className="md:flex gap-5 py-2">
+        <div className="md:w-1/2">
+          <div className="flex gap-3 bg-slate-300 w-36 px-2 items-center rounded-full">
+            <p className="text-sm font-medium text-slate-900">Bike Delivery</p>
+            <img src={cycle} className="h-7" />
           </div>
-            <h2 className='text-4xl md:text-7xl font-bold py-3'>The Fasted Delivery  in <span className='text-red-600'>Your Home</span></h2>
-            <p className='py-3 text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quam odit quo nihil architecto repellat ducimus eum nobis, tenetur, assumenda nostrum ea ex. Voluptatem soluta atque eos nemo culpa tempore.</p>
-            <button className='font-bold bg-red-500 text-slate-200 px-4 py-2 rounded-md'>Order Now</button>
+          <h2 className="text-4xl md:text-7xl font-bold py-3">
+            The Fasted Delivery in{" "}
+            <span className="text-red-600">Your Home</span>
+          </h2>
+          <p className="py-3 text-base">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quam
+            odit quo nihil architecto repellat ducimus eum nobis, tenetur,
+            assumenda nostrum ea ex. Voluptatem soluta atque eos nemo culpa
+            tempore.
+          </p>
+          <button className="font-bold bg-red-500 text-slate-200 px-4 py-2 rounded-md">
+            Order Now
+          </button>
         </div>
 
-        <div className='md:w-1/2 flex flex-wrap gap-5 p-4 justify-center'>
-          {
-           homeProductCartList[0] && homeProductCartList.map((el: any) =>{
+        <div className="md:w-1/2 flex flex-wrap gap-5 p-4 justify-center">
+          {homeProductCartList[0] ?
+            homeProductCartList.map((el: any) => {
+              return (
+                <HomeCard
+                  key={el._id}
+                  name={el.name}
+                  image={el.image}
+                  price={el.price}
+                  category={el.category}
+                />
+              );
+            })
+            : 
+            loadingArray.map((el:any,index)=>{
               return(
                 <HomeCard
-                key={el._id}
-                name={el.name}
-                image={el.image}
-                price={el.price}
-                category={el.category}
+                key={index}
+                loading="Loading..."
                 />
               )
             })
           }
         </div>
       </div>
-      <div className='mb-48'>
-          <h2 className='font-bold text-2xl text-slate-800 '>Fresh Vegitables</h2>
-          <div className=''>
-            {
-              homeProductCartListVegitables.map((el:any)=>{
-                return(
-                  <CardFeature
-                  key={el._id}
-                  name={el.name}
-                  image={el.image}
-                  price={el.price}
-                  category={el.category}
-                  />
-                )
-              })
-            }
-          </div>
+      <div className="mb-48">
+        <h2 className="font-bold text-2xl text-slate-800 ">Fresh Vegitables</h2>
+        <div className="">
+          {homeProductCartListVegitables.map((el: any) => {
+            return (
+              <CardFeature
+                key={el._id}
+                name={el.name}
+                image={el.image}
+                price={el.price}
+                category={el.category}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
